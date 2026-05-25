94e9ea50d5e1845f7ae57b704b9b4ae2e9086c9a
HenryS7ou9 <hnerique62@gmail.com>
2026-05-25 12:17:44 -0100

    Implement basic command interface in main.py

---
+++ brain/main.py
@@ -0,0 +1,17 @@
+print("AMBROSS ONLINE")
+
+while True:
+    comando = input("Você: ")
+
+    if comando == "olá":
+        print("Ambross: Sistema operacional.")
+
+    elif comando == "hora":
+        print("Ambross: Ainda não implementei relógio.")
+
+    elif comando == "sair":
+        print("Ambross: Encerrando.")
+        break
+
+    else:
+        print("Ambross: Comando desconhecido.")
